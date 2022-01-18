a5062237caabcdf8f49f695ab3d2b2ab81af27da
svnday <40030806+svnday@users.noreply.github.com>
2022-01-18 16:13:46 -0600

    added json parsing to server js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 const { animals } = require('./data/animals.json');
 
 
@@ -63,6 +67,12 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+});
+
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
